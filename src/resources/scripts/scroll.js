53c75fdbd64f31e7f6cf831a7af134f727034472
Artemy <kulbakowork@mail.ru>
2020-07-01 02:03:35 +0300

    Р­С„С„РµРєС‚ С…СЂРѕРјР°С‚РёС‡РµСЃРєРѕР№ Р°Р±Р±РµСЂР°С†РёРё; РЅР°РїРѕР»РЅРµРЅРёРµ РЅРµРєРѕС‚РѕСЂС‹С… СЃРµРєС†РёР№; СѓР»СѓС‡С€РµРЅРёСЏ СЃС‚РёР»СЏ

---
+++ src/resources/scripts/scroll.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict"
+
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(() => {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+            location.hostname === this.hostname) {
+            let target = $(this.hash)
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+            if (target.length) {
+                $('html, body').animate({ scrollTop: (target.offset().top - 48) }, 1000, "easeInOutExpo")
+                return false
+            }
+        }
+    })
+
+    $('.js-scroll-trigger').click(() => { $('.navbar-collapse').collapse('hide') })
+    $('body').scrollspy({ target: '#mainNav', offset: 54 })
+
+    const selector = "#mainNav"
+    const navBarCollapse = function() {
+        if ($(selector).offset().top > 100) $(selector).addClass("navbar-shrink")
+        else $(selector).removeClass("navbar-shrink")
+    }
+
+    navBarCollapse()
+    $(window).scroll(navBarCollapse)
+})(jQuery)
+
+//TODO жокер и локализация
+//TODO: пугалка
